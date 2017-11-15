c51a2b5f474bbd7feb487daea97d95c8002498d7
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2017-11-15 15:47:48 -0500

    AL: padding pour petit ecran

--- aspCoreTest/ClientApp/components/NavMenu.tsx
+++ aspCoreTest/ClientApp/components/NavMenu.tsx
@@ -3,32 +3,34 @@ import { Link, NavLink } from 'react-router-dom';
 
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
-        return <nav className='navbar navbar-inverse navbar-static-top'>
-            <div className='navbar-header'>
-                <Link className='navbar-brand' to={'/'}>aspCoreTest</Link>
-            </div>
-            <ul className='nav navbar-nav'>
-                <li>
-                    <NavLink to={'/'} exact activeClassName='active'>
-                        <span className='glyphicon glyphicon-home'></span> Home
+        return <div className="nav-menu">
+            <nav className='navbar navbar-inverse navbar-static-top'>
+                <div className='navbar-header'>
+                    <Link className='navbar-brand' to={'/'}>aspCoreTest</Link>
+                </div>
+                <ul className='nav navbar-nav'>
+                    <li>
+                        <NavLink to={'/'} exact activeClassName='active'>
+                            <span className='glyphicon glyphicon-home'></span> Home
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/counter'} activeClassName='active'>
-                        <span className='glyphicon glyphicon-education'></span> Counter
+                    </li>
+                    <li>
+                        <NavLink to={'/counter'} activeClassName='active'>
+                            <span className='glyphicon glyphicon-education'></span> Counter
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/fetchdata'} activeClassName='active'>
-                        <span className='glyphicon glyphicon-th-list'></span> Fetch data
+                    </li>
+                    <li>
+                        <NavLink to={'/fetchdata'} activeClassName='active'>
+                            <span className='glyphicon glyphicon-th-list'></span> Fetch data
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/Test1'} activeClassName='active'>
-                        <span className='glyphicon glyphicon-search'></span> Test1
+                    </li>
+                    <li>
+                        <NavLink to={'/Test1'} activeClassName='active'>
+                            <span className='glyphicon glyphicon-search'></span> Test1
                     </NavLink>
-                </li>
-            </ul>
-        </nav>;
+                    </li>
+                </ul>
+            </nav>
+        </div>;
     }
 }
