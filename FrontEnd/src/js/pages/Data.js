8c1b33fd6a7f20d9dccb27fd9cfc32db0f10eafd
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2017-11-22 15:25:16 -0500

    AL: fetch data from api

--- FrontEnd/src/js/pages/Data.js
+++ FrontEnd/src/js/pages/Data.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 export default class Data extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: "null"
+    }
+  }
+
   componentDidMount() {
-    fetch('http://teamguenonwebapi.azurewebsites.net/api/todos')
-    .then(data => {
-      console.log(data);
-    })
+    $.getJSON("http://teamguenonwebapi.azurewebsites.net/api/todos", function(json){
+      console.log(json);
+      var names = "";
+      for(var i = 0; i < 10; i++){
+        names = names + json[i].name + ", ";
+      }
+      this.setState({
+        data: names
+      });
+    }.bind(this));
   }
 
   render() {
@@ -14,6 +27,7 @@ export default class Data extends React.Component {
       <div>
         <h1>Data</h1>
         <div class="row">
+          {this.state.data}
         </div>
       </div>
     );
