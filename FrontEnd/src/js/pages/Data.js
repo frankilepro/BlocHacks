9f4b11c729b7ece463025727700c84e53eb3cc94
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2017-11-22 22:32:24 -0500

    DAta

--- FrontEnd/src/js/pages/Data.js
+++ FrontEnd/src/js/pages/Data.js
@@ -4,21 +4,72 @@ export default class Data extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: "null"
+      data: "",
+      obj: {
+        "name":""
+      }
     }
   }
 
   componentDidMount() {
-    $.getJSON("http://teamguenonwebapi.azurewebsites.net/api/todos", function(json){
-      console.log(json);
-      var names = "";
-      for(var i = 0; i < 10; i++){
+    fetch('http://teamguenonwebapi.azurewebsites.net/api/todos')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      /*console.log(json);
+      let names = "";
+      for(var i = 0; i < json.length; i++){
         names = names + json[i].name + ", ";
-      }
+      }*/
       this.setState({
-        data: names
-      });
-    }.bind(this));
+        data: json
+      })
+    }.bind(this))
+  }
+
+  showData(){
+    let rows = [];
+    let data = this.state.data;
+    for(var i = 0; i < data.length; i++){
+      let text = "name: " + data[i].name + ", id : " + data[i].id;
+      rows.push(<h1 key={i} style={{backgroundColor:"yellow"}}>{text}<br></br></h1>);
+    }
+    return <div>{rows}</div>;
+  }
+
+  onSubmit = (e) => {
+    this.state.obj.name = $('#input-name').val();
+    console.log(this.state.obj);
+
+    /*fetch("http://teamguenonwebapi.azurewebsites.net/api/todos", {
+      method:"post",
+      "crossDomain": true,
+      header : {
+        'Accept': 'application/json, text/plain',
+        'Content-Type': 'application/json',
+        "Acces-Control-Allow-Origin": "*"
+      },
+      data: this.state.obj
+    });*/
+
+
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://teamguenonwebapi.azurewebsites.net/api/todos",
+      "method": "POST",
+      "dataType" : "json",
+      "headers": {
+        "Acces-Control-Allow-Origin": "*",
+        "content-type":"application/json"
+      },
+      "data": this.state.obj
+    }
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
   }
 
   render() {
@@ -26,8 +77,12 @@ export default class Data extends React.Component {
     return (
       <div>
         <h1>Data</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>Name: <input type="text" id="input-name" onchange={this.updateName}/></div>
+          <button type="submit">Post</button>
+        </form>
         <div class="row">
-          {this.state.data}
+          {this.showData()}
         </div>
       </div>
     );
