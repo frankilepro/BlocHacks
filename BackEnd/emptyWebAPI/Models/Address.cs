5d4ab032de805f14d72303f8db9d322655fc46fd
yoangau <31855017+yoangau@users.noreply.github.com>
2017-11-25 17:52:16 -0500

    vide

---
+++ BackEnd/emptyWebAPI/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamGuenonWebApi.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        public int Number { get; set; }
+        public int Appartment { get; set; }
+        public string Street { get; set; }
+        public string Postal { get; set; }
+        public string Type { get; set; } //Street, avenue, etc.
+
+    }
+}
