0bc5e3c2cdf2047ebf5582c6dcda860504d48c1b
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2017-11-25 18:56:44 -0500

    modify page

--- FrontEnd/src/js/pages/RefugeeProfile.js
+++ FrontEnd/src/js/pages/RefugeeProfile.js
@@ -6,9 +6,15 @@ GoogleMapsLoader.LANGUAGE='en';
 GoogleMapsLoader.REGION='CA';
 
 export default class RefugeeProfile extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            modifying : false
+        }
+    }
+
     componentDidMount(){
-
-
+        $(".modify-info").hide();
         GoogleMapsLoader.load(function(google){
             var poly_coordinates=new google.maps.LatLng(45.5044, -73.6129); //enleve la ligne
             var map = new google.maps.Map(document.getElementById('map'), {
@@ -22,8 +28,21 @@ export default class RefugeeProfile extends React.Component {
         });
     }
 
-// <div id="map">
-// </div>
+    modify() {
+        if($(".modify-info").is(":hidden")){
+            $(".modify-info").show();
+            $(".show-info").hide();
+        }
+        else{
+            $(".modify-info").hide();
+            $(".show-info").show();
+        }
+    }
+
+    update (){
+        console.log("sent");
+    }
+
   render() {
     return (
     <div>
@@ -31,12 +50,20 @@ export default class RefugeeProfile extends React.Component {
         <br></br>
         <br></br>
         <div class="row" style={{paddingBottom:"50px"}}>
-            <div class="col-sm-6 brdright">
-                <div class="info">Address : (refugee address)</div>
+            <div class="col-sm-6 brdright show-info">
+                <div class="info">Address : (boogie address)</div>
                 <div class="info">E-mail : (refugee email)</div>
                 <div class="info">Phone number : (refugee phone)</div>
                 <br></br>
-                <div class="btn btn-default">Modify my informations</div>
+                <button class="btn btn-default" onClick={this.modify}>Modify my informations</button>
+            </div>
+            <div class="col-sm-6 brdright modify-info">
+                <div class="info">Address : <input placeholder="(refugee address)"></input></div>
+                <div class="info">E-mail : <input placeholder="(refugee email)"></input></div>
+                <div class="info">Phone number : <input placeholder="(refugee number)"></input></div>
+                <br></br>
+                <button class="btn btn-default" onClick={this.modify}>Cancel</button>
+                <button class="btn btn-default" onClick={this.update}>Update</button>
             </div>
             <div class="col-sm-6">
                 <div class="info">Refugee center : (refugee center)</div>
