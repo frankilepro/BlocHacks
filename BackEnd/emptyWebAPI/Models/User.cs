b94ccb8d093a06d6ddb882ff6e33835e9efc93c1
olivierlauzon85 <olivierlauzon85@gmail.com>
2017-11-25 17:52:54 -0500

    User pas fini

---
+++ BackEnd/emptyWebAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamGuenonWebApi.Models
+{
+    public class User
+    {
+        public Name Name { get; set; }
+        public Address UserAddress { get; set; }
+
+
+    }
+}
