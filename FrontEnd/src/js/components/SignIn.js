e3beb1685db763b4f07d9c4df9d23fab3046aed6
Philippe Trudel <p.trudel1996@gmail.com>
2017-11-26 01:57:18 -0500

    Auto stash before merge of "master" and "origin/master"

--- FrontEnd/src/js/components/SignIn.js
+++ FrontEnd/src/js/components/SignIn.js
@@ -4,21 +4,32 @@ import {Link} from 'react-router';
 export default class SignIn extends React.Component {
     render() {
         return (
-        <div>
-            <h1 style={{textAlign:"center"}}>Sign in as :</h1>
-            <div class="row">
-                <div class="col-sm-6">
-                    <Link to="/refugeformpage">
-                        <div id='signin'> Refugee </div>
-                    </Link>
-                </div>
-                <div class="col-sm-6">
-                    <Link to="/centerformpage">
-                        <div id='signin'> Center </div>
-                    </Link>
+
+            <div class="jumbotron jumbotron-background">
+            <div class="container">
+                <div>
+                <h1 style={{textAlign:"center"}}>Sign in as :</h1>
+                <div class="row">
+                <div class="col-sm-3"></div>
+                    <div class="col-sm-3">
+                        <Link to="/refugeformpage">
+                            <div className="signIn">
+                                <div className="verticalAlign">Refugee</div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div class="col-sm-3">
+                        <Link to="/centerformpage">
+                            <div className="signIn">
+                                <div className="verticalAlign">Center</div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div class="col-sm-3"></div>
                 </div>
             </div>
-        </div>
+            </div>
+          </div>
         );
   }
 }
