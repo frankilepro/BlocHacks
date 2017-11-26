0ce88e44e16dca5566bb668964cc6361deb7e46a
Philippe Trudel <p.trudel1996@gmail.com>
2017-11-26 08:19:45 -0500

    footer

--- FrontEnd/src/js/components/layout/Footer.js
+++ FrontEnd/src/js/components/layout/Footer.js
@@ -7,14 +7,16 @@ export default class Footer extends React.Component {
       <footer class="footer">
           <div class="col-sm-3"></div>
           <div class="col-sm-6">
-            <div class="col-sm-6 footer-padding footer-content">
+            <div class="col-sm-5 footer-padding footer-content">
               <p>1610 RUE SAINTE-CATHERINE OUEST,</p>
-              <p>MONTRÉAL, QC,</p>
-              <p>H3H 2S2,</p>
-              <p>CANADA</p>
+              <p>MONTRÉAL, QC, H3H 2S2, CANADA</p>
+              <p>514-846-0005</p>
             </div>
-            <div class="col-sm-6 footer-content">
-
+            <div class="col-sm-7 footer-padding footer-content">
+              <p>Partner</p>
+              <hr/>
+              <p>Refugee Center | https://www.therefugeecentre.org/</p>
+              <p>UN Refugee Angency | https://www.unhcr.ca</p>
             </div>
           </div>
           <div class="col-sm-3"></div>
